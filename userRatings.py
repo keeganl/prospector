09c28a836168c0746778b64abb52df2f3c36b174
Keegan Lawley <keeganlk.7@gmail.com>
2021-03-25 19:28:52 -0500

    extracting metadata and getting the model to train linear regression

--- userRatings.py
+++ userRatings.py
@@ -6,6 +6,7 @@ import time
 import datetime
 import pandas as pd
 import xlsxwriter
+import json
 
 totalUsers = 99179
 totalMovies = 2614
@@ -19,6 +20,12 @@ class Weight:
     def __init__(self, index, val):
         self.index = index
         self.val = val
+
+class MovieMetadata:
+    def __init__(self, id, metadata):
+        self.id = id
+        self.metadata = metadata
+
 
 def buildArray(filename):
     with open(filename) as reader:
@@ -36,10 +43,40 @@ def buildArray(filename):
             line = reader.readline()
     return np.squeeze(arr)
 
+
+def getMetadata():
+    revenueEdited = pd.read_csv("./utils/revenueEditedreIDed.csv")
+    print("Revenue Movie dataset")
+    
+    obj = dict()
+    arr = []
+    for index, row in revenueEdited.iterrows():
+        obj = {
+            'title': row.title, 
+            'budget': row.budget,
+            'popularity': row.popularity, 
+            'revenue': row.revenue,
+            'runtime': row.runtime,
+            'release_date': row.release_date,
+            'vote_average': row.vote_average, 
+            'vote_count': row.vote_count,
+            'genres': []}
+        m = MovieMetadata(row.id, obj)
+        genreArr = []
+        genreObj = row.genres.replace("'", '"')
+        for g in json.loads(genreObj):
+            genreArr.append(g["name"])
+        obj['genres'] = genreArr
+        # print( obj['genres'])
+        arr.append(m)
+    return arr
+
+
 tStart = time.perf_counter()
 
 arr_i = buildArray('i.quadratic') 
 arr_u  = buildArray('u.quadratic') 
+allMovieMetadata = getMetadata()
 
 dotProds = []
 watchedMovies = []
@@ -49,13 +86,30 @@ for i in range(0, len(arr_i)):
     dotProds.append(x)
     watchedMovies.append((arr_u[i].id, arr_i[i].id))
 
+
+# build 0-1 matrix (extremely sparse)
 seenMovies = np.zeros((totalUsers,totalMovies))
 for watch in watchedMovies:
-    print(str(watch[0]) + "\t" + str(watch[1]))
-    seenMovies[watch[0]-1 ][watch[1]-1] = 1
+    # print(str(watch[0]) + "\t" + str(watch[1]))
+    seenMovies[watch[0]-1][watch[1]-1] = 1
+        
+print(len(allMovieMetadata))
 
-df = pd.DataFrame (seenMovies)
-print(df)
+# assign metadata for linear regression, this model is 30 GB!!
+with open('data.vw', 'w') as f:
+    for i in range(totalUsers):
+        print("processing user: " + str(i) + " ...")
+        for j in range(totalMovies):
+            # print(i , j)
+            # print("J", j)
+            m = allMovieMetadata[j-1].metadata
+            # print("M", m)
+            data = str(seenMovies[i][j]) + ' | ' + str(m['budget']) + ' ' + str(m['popularity']) + ' ' + str(m['revenue']) + ' ' + str(m['runtime']) + ' ' + str(m['release_date']) + ' ' + str(m['vote_average']) + ' ' + str(m['vote_count']) + ' ' + " ".join(m['genres']) + "\n"
+            f.write(data)
+        
+
+# df = pd.DataFrame (seenMovies)
+# print(df)
 # filepath = 'my_excel_file.xlsx'
 
 # df.to_excel(filepath, index=False)
